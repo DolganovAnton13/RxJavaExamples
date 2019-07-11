0c6970711482bf49c9a81a8e7f12f5462b64659a
Antoha1397 <toscha.17@mail.ru>
2019-07-11 19:52:42 +0500

    example 3

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -12,6 +12,7 @@ import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,43 +22,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Если у вас есть синхронный метод, который вам надо сделать асинхронным, то оператор fromCallable поможет вам
-        Observable.fromCallable(new CallableLongAction("5"))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<Integer>() {
-                    @Override
-                    public void call(Integer integer) {
-                        Log.i("Tag","onNext " + integer);
-                    }
-                });
+        //Оператор map преобразует все элементы последовательности.
+        // Для этого нам необходимо написать функцию преобразования.
+        // Например конвертация из String в Integer.
+        
+        Observable<Integer> observable = Observable
+                .from(new String[]{"1", "2", "3", "4", "5", "6"})
+                .map(stringToInteger);
+
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+                Log.i("tag", "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i("tag", "onError: " + e);
+            }
+
+            @Override
+            public void onNext(Integer s) {
+                Log.i("tag", "onNext: " + s);
+            }
+        };
+
+
+        observable.subscribe(observer);
     }
 
-
-}
-
-class CallableLongAction implements Callable<Integer> {
-
-    private final String data;
-
-    public CallableLongAction(String data) {
-        this.data = data;
-    }
-
-    @Override
-    public Integer call() throws Exception {
-        return longAction(data);
-    }
-
-    private int longAction(String text) {
-        Log.d("Tag","longAction");
-
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    Func1<String, Integer> stringToInteger = new Func1<String, Integer>() {
+        @Override
+        public Integer call(String s) {
+            return Integer.parseInt(s);
         }
-
-        return Integer.parseInt(text);
-    }
+    };
 }
+
